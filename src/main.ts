58000b69e4b720fb5213201dbf82b2c265293e6d
Heiko Beineke <heiko.beineke@gmail.com>
2021-06-27 16:39:36 +0200

    Add i18n

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,6 @@ import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
 import './assets/styles/main.scss';
+import i18n from './i18n';
 
-createApp(App).use(router).mount('#app');
+createApp(App).use(i18n).use(router).mount('#app');
--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,16 @@ module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/cv/'
     : '/',
+
+  pluginOptions: {
+    i18n: {
+      locale: 'de',
+      fallbackLocale: 'en',
+      localeDir: 'locales',
+      enableLegacy: false,
+      runtimeOnly: false,
+      compositionOnly: false,
+      fullInstall: true,
+    },
+  },
 };
