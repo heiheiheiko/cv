2454228a97a9603639792f810bcdbd8abdb427bb
Heiko Beineke <heiko.beineke@gmail.com>
2021-06-30 20:00:11 +0200

    Add slot to AppLayout and use it in HomePage

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,15 +1,26 @@
 <template>
-  <AppLayout />
+  <AppLayout>
+    <BaseSection>
+      <BaseSectionTitle> Title </BaseSectionTitle>
+      <BaseSectionBody> Content </BaseSectionBody>
+    </BaseSection>
+  </AppLayout>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import AppLayout from '@/components/AppLayout.vue';
+import BaseSection from '@/components/BaseSection.vue';
+import BaseSectionTitle from '@/components/BaseSectionTitle.vue';
+import BaseSectionBody from '@/components/BaseSectionBody.vue';
 
 export default defineComponent({
-  name: 'Home',
+  name: 'HomePage',
   components: {
     AppLayout,
+    BaseSection,
+    BaseSectionTitle,
+    BaseSectionBody,
   },
 });
 </script>
