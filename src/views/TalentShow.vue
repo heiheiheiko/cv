f692ecdd3d8448bf0f71ab97864f4b5072c5259f
Heiko Beineke <heiko.beineke@gmail.com>
2021-07-06 23:00:54 +0200

    Use TalentLayout in TalentShow

--- src/views/TalentShow.vue
+++ src/views/TalentShow.vue
@@ -1,47 +1,27 @@
 <template>
-  <AppLayout>
-    <TraitSection :traits="traits" />
-    <pre>
-      {{ talent }}
-    </pre>
-    <BaseSection>
-      <BaseSectionTitle> Title </BaseSectionTitle>
-      <BaseSectionBody> Content </BaseSectionBody>
-    </BaseSection>
-  </AppLayout>
+  <TalentLayout />
 </template>
 
 <script lang="ts">
 import { defineComponent, computed } from 'vue';
 import { useRoute } from 'vue-router';
-import AppLayout from '@/components/AppLayout.vue';
-import BaseSection from '@/components/BaseSection.vue';
-import BaseSectionTitle from '@/components/BaseSectionTitle.vue';
-import BaseSectionBody from '@/components/BaseSectionBody.vue';
+import TalentLayout from '@/components/TalentLayout.vue';
 import useTalent from '@/composables/useTalent';
-import useTrait from '@/composables/useTrait';
-import TraitSection from '@/components/TraitSection.vue';
 
 export default defineComponent({
   name: 'HomePage',
   components: {
-    AppLayout,
-    BaseSection,
-    BaseSectionTitle,
-    BaseSectionBody,
-    TraitSection,
+    TalentLayout,
   },
   setup() {
     const route = useRoute();
     const { findTalent, fetchTalents } = useTalent();
-    const { findTraits } = useTrait();
 
     const talentId = parseInt(route.params.id as string, 10);
     const talent = computed(() => findTalent(talentId));
-    const traits = computed(() => findTraits(talent.value.traits));
 
     return {
-      findTalent, fetchTalents, talent, traits,
+      findTalent, fetchTalents, talent,
     };
   },
   created() {
