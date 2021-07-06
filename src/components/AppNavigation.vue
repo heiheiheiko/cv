729ea77295a60b936d521573bafde2bb61fd507c
Heiko Beineke <heiko.beineke@gmail.com>
2021-07-06 18:20:21 +0200

    Replace AppNavigation

--- src/components/AppNavigation.vue
+++ src/components/AppNavigation.vue
@@ -1,105 +1,143 @@
-<!-- This example requires Tailwind CSS v2.0+ -->
 <template>
-  <Disclosure
-    v-slot="{ isOpen }"
-    as="nav"
-    class="bg-white border-b border-gray-200"
+  <Popover
+    open="true"
+    class="relative bg-white"
   >
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-      <div class="flex justify-between h-16">
-        <div class="flex">
-          <div class="flex-shrink-0 flex items-center">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6">
+      <div
+        class="flex justify-between items-center border-b-2 border-gray-100
+                  py-6 md:justify-start md:space-x-10"
+      >
+        <div class="flex justify-start lg:w-0 lg:flex-1">
+          <a href="#">
+            <span class="sr-only">Workflow</span>
             <img
-              class="block lg:hidden h-8 w-auto"
+              class="h-8 w-auto sm:h-10"
               src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-              alt="Workflow"
+              alt=""
             >
-            <img
-              class="hidden lg:block h-8 w-auto"
-              src="https://tailwindui.com/img/logos/workflow-logo-indigo-600-mark-gray-800-text.svg"
-              alt="Workflow"
-            >
-          </div>
-          <div class="hidden sm:-my-px sm:ml-6 sm:flex sm:space-x-8">
-            <AppNavigationLink
-              v-for="object in navigation"
-              :key="object.name"
-              :navigation-object="object"
-            />
-          </div>
+          </a>
         </div>
-        <div class="hidden sm:ml-6 sm:flex sm:items-center">
-          <button
-            class="bg-white p-1 rounded-full text-gray-400 hover:text-gray-500
-            focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        <div class="-mr-2 -my-2 md:hidden">
+          <PopoverButton
+            class="bg-white rounded-md p-2 inline-flex items-center
+                   justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100
+                   focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
           >
-            <span class="sr-only">View notifications</span>
-            <BellIcon
+            <span class="sr-only">Open menu</span>
+            <MenuIcon
               class="h-6 w-6"
               aria-hidden="true"
             />
-          </button>
+          </PopoverButton>
         </div>
-        <div class="-mr-2 flex items-center sm:hidden">
-          <!-- Mobile menu button -->
-          <DisclosureButton
-            class="bg-white inline-flex items-center justify-center p-2
-            rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none
-            focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        <PopoverGroup
+          as="nav"
+          class="hidden md:flex space-x-10"
+        >
+          <a
+            v-for="object in navigation"
+            :key="object.name"
+            href="#"
+            class="text-base font-medium text-gray-500 hover:text-gray-900"
           >
-            <span class="sr-only">isOpen main menu</span>
-            <MenuIcon
-              v-if="!isOpen"
-              class="block h-6 w-6"
-              aria-hidden="true"
-            />
-            <XIcon
-              v-else
-              class="block h-6 w-6"
-              aria-hidden="true"
-            />
-          </DisclosureButton>
+            {{ object.name }}
+          </a>
+        </PopoverGroup>
+
+        <div class="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
+          <!-- social media ?! -->
         </div>
       </div>
     </div>
 
-    <DisclosurePanel class="sm:hidden">
-      <div class="pt-2 pb-3 space-y-1">
-        <AppNavigationLinkMobile
-          v-for="object in navigation"
-          :key="object.name"
-          :navigation-object="object"
-        />
-      </div>
-    </DisclosurePanel>
-  </Disclosure>
+    <transition
+      enter-active-class="duration-200 ease-out"
+      enter-from-class="opacity-0 scale-95"
+      enter-to-class="opacity-100 scale-100"
+      leave-active-class="duration-100 ease-in"
+      leave-from-class="opacity-100 scale-100"
+      leave-to-class="opacity-0 scale-95"
+    >
+      <PopoverPanel
+        focus
+        class="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"
+      >
+        <div
+          class="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5
+                  bg-white divide-y-2 divide-gray-50"
+        >
+          <div class="pt-5 pb-6 px-5">
+            <div class="flex items-center justify-between">
+              <div>
+                <img
+                  class="h-8 w-auto"
+                  src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+                  alt="Workflow"
+                >
+              </div>
+              <div class="-mr-2">
+                <PopoverButton
+                  class="bg-white rounded-md p-2 inline-flex items-center justify-center
+                         text-gray-400 hover:text-gray-500 hover:bg-gray-100
+                         focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+                >
+                  <span class="sr-only">Close menu</span>
+                  <XIcon
+                    class="h-6 w-6"
+                    aria-hidden="true"
+                  />
+                </PopoverButton>
+              </div>
+            </div>
+            <div class="mt-6">
+              <nav class="grid gap-y-8">
+                <a
+                  v-for="object in navigation"
+                  :key="object.name"
+                  href="#"
+                  class="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50"
+                >
+                  <SupportIcon
+                    class="flex-shrink-0 h-6 w-6 text-indigo-600"
+                    aria-hidden="true"
+                  />
+                  <span class="ml-3 text-base font-medium text-gray-900">
+                    {{ object.name }}
+                  </span>
+                </a>
+              </nav>
+            </div>
+          </div>
+        </div>
+      </PopoverPanel>
+    </transition>
+  </Popover>
 </template>
 
 <script>
-import { defineComponent, ref } from 'vue';
-
+import { defineComponent } from 'vue';
 import {
-  Disclosure, DisclosureButton, DisclosurePanel,
+  Popover, PopoverButton, PopoverGroup, PopoverPanel,
 } from '@headlessui/vue';
-import { BellIcon, MenuIcon, XIcon } from '@heroicons/vue/outline';
+import {
+  MenuIcon,
+  SupportIcon,
+  XIcon,
+} from '@heroicons/vue/outline';
 import { useI18n } from 'vue-i18n';
-import AppNavigationLink from '@/components/AppNavigationLink.vue';
-import AppNavigationLinkMobile from '@/components/AppNavigationLinkMobile.vue';
 
 export default defineComponent({
   components: {
-    Disclosure,
-    DisclosureButton,
-    DisclosurePanel,
-    BellIcon,
+    Popover,
+    PopoverButton,
+    PopoverGroup,
+    PopoverPanel,
     MenuIcon,
+    SupportIcon,
     XIcon,
-    AppNavigationLink,
-    AppNavigationLinkMobile,
   },
   setup() {
-    const isOpen = ref(false);
-
     const { t } = useI18n({
       inheritLocale: true,
       useScope: 'local',
@@ -113,7 +151,6 @@ export default defineComponent({
 
     return {
       navigation,
-      isOpen,
       t,
     };
   },
