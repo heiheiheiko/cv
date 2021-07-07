01cef1342025cfb83b0c5d16d6fc0641fb8fa901
Heiko Beineke <heiko.beineke@gmail.com>
2021-07-07 23:21:50 +0200

    Add HeroIcon

---
+++ src/components/HeroIcon.vue
@@ -0,0 +1,17 @@
+<template>
+  <component :is="icon" />
+</template>
+
+<script>
+import * as components from '@heroicons/vue/solid';
+
+export default {
+  components,
+  props: {
+    icon: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
