af3b2e08b9ccc6fe6ece891c09dcd976a5fad914
Heiko Beineke <heiko.beineke@gmail.com>
2021-07-07 23:22:42 +0200

    Use HeroIcon for BaseCardFeatureIcon

--- src/components/BaseCardFeatureIcon.vue
+++ src/components/BaseCardFeatureIcon.vue
@@ -4,8 +4,8 @@
       class="inline-flex items-center justify-center p-3 bg-gradient-to-r
            from-teal-500 to-cyan-600 rounded-md shadow-lg"
     >
-      <component
-        :is="icon"
+      <HeroIcon
+        :icon="icon"
         class="h-6 w-6 text-white"
         aria-hidden="true"
       />
@@ -15,8 +15,12 @@
 
 <script>
 import { defineComponent } from 'vue';
+import HeroIcon from '@/components/HeroIcon.vue';
 
 export default defineComponent({
+  components: {
+    HeroIcon,
+  },
   props: {
     icon: {
       type: Function,
