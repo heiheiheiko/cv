7032e63a5523754ea0f36586d6bdb85e52f329a7
Heiko Beineke <heiko.beineke@gmail.com>
2021-07-08 22:43:58 +0200

    Add font awesome

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { createApp } from 'vue';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
 import './assets/styles/main.scss';
 import i18n from './i18n';
 
-createApp(App).use(i18n).use(router).mount('#app');
+library.add(faUserSecret);
+
+const app = createApp(App).use(i18n).use(router);
+app.component('FontAwesomeIcon', FontAwesomeIcon);
+
+app.mount('#app');
