2eeda338670c2ce962c0354d7a6c32ad3ca00200
Heiko Beineke <heiko.beineke@gmail.com>
2021-07-11 22:28:59 +0200

    Add BaseAvatar

---
+++ src/components/BaseAvatar.vue
@@ -0,0 +1,36 @@
+<template>
+  <div
+    class="flex items-center justify-center rounded-full"
+    :class="[sizeClass[size], `bg-${color}-500`]"
+  >
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+const sizeClass = {
+  tiny: 'h-6 w-6',
+  small: 'h-8 w-8',
+  normal: 'h-10 w-10',
+  large: 'h-12 w-12',
+  huge: 'h-14 w-14',
+} as any;
+
+export default defineComponent({
+  props: {
+    color: {
+      type: String,
+      default: 'gray',
+    },
+    size: {
+      type: String,
+      default: 'normal',
+    },
+  },
+  setup() {
+    return { sizeClass };
+  },
+});
+</script>
