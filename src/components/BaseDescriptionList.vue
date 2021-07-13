ac42d08902ebd4b56c1e60fd1adaacc35ca66f4e
Heiko Beineke <heiko.beineke@gmail.com>
2021-07-13 22:39:14 +0200

    Add BaseDescriptionList

---
+++ src/components/BaseDescriptionList.vue
@@ -0,0 +1,5 @@
+<template>
+  <dl class="space-y-8 px-4 sm:px-6 sm:space-y-6">
+    <slot />
+  </dl>
+</template>
---
+++ src/components/BaseDescriptionTerm.vue
@@ -0,0 +1,5 @@
+<template>
+  <dd class="mt-1 text-sm text-gray-900 sm:col-span-2">
+    <slot />
+  </dd>
+</template>
