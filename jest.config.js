982d506614f0906297f592fff35adf46584da4e1
Heiko Beineke <heiko.beineke@gmail.com>
2021-07-28 23:51:17 +0200

    Add coverage to test:unit script

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,11 @@ module.exports = {
   transform: {
     '^.+\\.vue$': 'vue-jest',
   },
+  collectCoverageFrom: [
+    'src/components/**/*.{vue, ts}',
+    'src/composables/**/*.{ts, js}',
+    'src/decorators/**/*.{ts, js}',
+    'src/services/**/*.{ts, js}',
+    'src/utils/**/*.{ts, js}',
+  ],
 };
