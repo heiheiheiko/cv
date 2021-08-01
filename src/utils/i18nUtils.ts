e0cfbc8bf1a0c1f9ae9a27b396f8e413f8375467
Heiko Beineke <heiko.beineke@gmail.com>
2021-08-01 16:06:16 +0200

    Refactor I18nUtils to be testable

--- src/utils/i18nUtils.ts
+++ src/utils/i18nUtils.ts
@@ -1,12 +1,11 @@
 /* eslint-disable import/prefer-default-export */
 
 import { I18nField } from '@/db/dbTypes';
-import { useI18n } from 'vue-i18n';
+import i18n from '@/i18n';
 
 function translateI18nField(i18nField: I18nField|undefined): string {
   if (!i18nField) { return ''; }
-  const { locale } = useI18n();
-  return i18nField[locale.value];
+  return i18nField[i18n.global.locale.value];
 }
 
 export {
